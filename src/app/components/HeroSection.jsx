b2698f4573e0c53107524b7234d7a08da10af08f
Joshua Cho <collegecho98@gmail.com>
2024-05-07 14:37:35 -0700

    update herosection
    changed the cursor style on download button

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -38,7 +38,7 @@ const HeroSection = () => {
             <a href="/josh-cho-resume-master.pdf" download="Jae-Hun-Cho.pdf">
               <button
                 type="button"
-                className="rounded-full font-bold text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 text-sm px-5 py-2.5 text-center"
+                className="hover:cursor-pointer rounded-full font-bold text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 text-sm px-5 py-2.5 text-center"
               >
                 Download CV
               </button>
